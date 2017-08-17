724b4ce3f7a0dfe796c9bc95141339aa6794bc00
Juliana Potengy <julianalpotengy@gmail.com>
2017-08-17 11:47:50 -0300

    Done

---
+++ MenuDamnPrison/Assets/MenuBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void LoadLevel (int i)
+    {
+        Application.LoadLevel(i);
+    }
+
+    public void PlaySelect(AudioClip clip)
+    {
+        AudioSource audio = Object.FindObjectOfType<AudioSource>() as AudioSource;
+        audio.PlayOneShot(clip);
+    }
+}
